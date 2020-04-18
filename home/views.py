628e37a5853d65000677657840ebadb1da259fe2
yilmazmertm <yilmazmertm@gmail.com>
2020-04-18 15:45:14 +0300

    implement mptt / I should check the navbar later - vid 15

--- home/views.py
+++ home/views.py
@@ -5,13 +5,14 @@ from django.contrib import messages
 
 # Create your views here.
 from home.models import Setting, ContactFormMessage, ContactFormu
-from product.models import Product
+from product.models import Product, Category
 
 
 def index(request):
     setting = Setting.objects.get(pk = 1)
     sliderdata = Product.objects.all()[:4]
-    context = {'setting': setting, 'page': 'home', 'sliderdata': sliderdata}
+    category = Category.objects.all()
+    context = {'setting': setting,'category' : category, 'page': 'home', 'sliderdata': sliderdata}
     return render(request, 'index.html', context)
 
 def aboutus(request):
--- product/models.py
+++ product/models.py
@@ -2,8 +2,11 @@ from django.db import models
 # Create your models here.
 from django.utils.safestring import mark_safe
 from ckeditor_uploader.fields import RichTextUploadingField
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
 
-class Category(models.Model):
+
+class Category(MPTTModel):
     STATUS = (('True', 'Evet'), ('False', 'Hayır'))
     title = models.CharField(max_length= 100)
     keywords = models.CharField(max_length=255)
@@ -11,11 +14,23 @@ class Category(models.Model):
     image = models.ImageField(blank= True, upload_to= 'images/')
     status = models.CharField(max_length=10, choices= STATUS)
     slug = models.SlugField()
-    parent = models.ForeignKey('self', blank= True, null= True, related_name= 'children', on_delete= models.CASCADE)
+    parent = TreeForeignKey('self', blank= True, null= True, related_name= 'children', on_delete= models.CASCADE)
     created_at = models.DateTimeField(auto_now_add= True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['title']
+
     def __str__(self):
-        return self.title
+        full_path = [self.title]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.title)
+            k = k.parent
+        return ' -> '.join(full_path[::1])
+
+
+
 
 class Product(models.Model):
     STATUS = (('True', 'Evet'), ('False', 'Hayır'))
@@ -47,6 +62,8 @@ class Images(models.Model):
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     image_tag.short_description = 'Image'
+
+
 
 
 
