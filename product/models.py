4bf1095237087104f30f561d30db04e6795d692d
yilmazmertm <yilmazmertm@gmail.com>
2020-05-08 22:09:13 +0300

    automated slug fields on category and product fields - vid 22

--- product/models.py
+++ product/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 # Create your models here.
 from django.forms import ModelForm
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 from ckeditor_uploader.fields import RichTextUploadingField
 from mptt.fields import TreeForeignKey
@@ -15,7 +16,7 @@ class Category(MPTTModel):
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
-    slug = models.SlugField()
+    slug = models.SlugField(null= False, unique= True)
     parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
@@ -31,6 +32,13 @@ class Category(MPTTModel):
             k = k.parent
         return ' -> '.join(full_path[::1])
 
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
 
 class Product(models.Model):
     STATUS = (('True', 'Evet'), ('False', 'Hayır'))
@@ -43,7 +51,7 @@ class Product(models.Model):
     amount = models.IntegerField(blank=True)
     detail = RichTextUploadingField(blank=True)
     status = models.CharField(max_length=10, choices=STATUS)
-    slug = models.SlugField(blank=True, max_length=150)
+    slug = models.SlugField(null= False, unique= True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
@@ -52,8 +60,13 @@ class Product(models.Model):
 
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
-
     image_tag.short_description = 'Image'
+
+    def catimg_tag(self):
+        return mark_safe((Category.status))
+
+    def get_absolute_url(self):
+        return reverse('productt_detail', kwargs={'slug': self.slug})
 
 
 class Images(models.Model):
